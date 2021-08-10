6010d4ba42e0e925544eb74853d9738e391ac82c
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-08-10 09:20:46 +0530

    install sass install react bootstrap install react icons Add favicon

---
+++ src/Client.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Col, Row} from "react-bootstrap";
+
+const Client: React.FC = () => {
+    return (
+        <Row>
+            <Col>
+                <h1 className='float-right'>Hello World</h1>
+            </Col>
+        </Row>
+
+    )
+}
+
+export default Client;
